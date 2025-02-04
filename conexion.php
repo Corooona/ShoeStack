b5cb5d5bf8c9962aee5a97c8302d438acad210b5
corona <mc23584100@gmail.com>
2025-02-03 19:42:35 -0600

    nueva rama

--- conexion.php
+++ conexion.php
@@ -1,6 +1,13 @@
 <!-- archivo 2 -->
 <?php
-include("configuracion.php");
+
+<?php
+$server="localhost";
+$user="root";
+$pass="";
+$db="shoestock";
+?>
+
 $conexion=mysqli_connect($server,$user,$pass,$db);
 
 if(mysqli_connect_errno()){
